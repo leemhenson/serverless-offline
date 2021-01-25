c1b6c435b41dc0743cd95ea01f2b2a69d550dde7
Lee Henson <lee.m.henson@gmail.com>
2021-01-25 11:53:54 +0000

    remove commonjs babel plugin

--- babel.config.js
+++ babel.config.js
@@ -8,9 +8,7 @@ module.exports = {
         loose: true,
       },
     ],
-    '@babel/plugin-proposal-dynamic-import',
     '@babel/plugin-proposal-nullish-coalescing-operator',
     '@babel/plugin-proposal-optional-chaining',
-    '@babel/plugin-transform-modules-commonjs',
   ],
 }
